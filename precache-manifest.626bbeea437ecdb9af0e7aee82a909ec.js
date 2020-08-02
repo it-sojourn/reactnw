53f713ae4206a6279359fb98e0f738371fb95b27
it-sojourn <romanovks1337@gmail.com>
2020-08-02 14:08:36 +0900

    Updates

---
+++ precache-manifest.626bbeea437ecdb9af0e7aee82a909ec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02a95db05bd5fd496c0523d1a06fbfd",
+    "url": "/reactnw/index.html"
+  },
+  {
+    "revision": "1817c2041e7b7b25ea75",
+    "url": "/reactnw/static/css/4.d186818e.chunk.css"
+  },
+  {
+    "revision": "f1baa4946ea71a615beb",
+    "url": "/reactnw/static/css/5.eb1a6840.chunk.css"
+  },
+  {
+    "revision": "b064deb6247167c0ef62",
+    "url": "/reactnw/static/css/6.6b6c8137.chunk.css"
+  },
+  {
+    "revision": "8e12c7b803c577e3e659",
+    "url": "/reactnw/static/css/7.f161759a.chunk.css"
+  },
+  {
+    "revision": "a8fac92571d05d63fa80",
+    "url": "/reactnw/static/css/main.14415e5b.chunk.css"
+  },
+  {
+    "revision": "6bd94e72c9b533d77ce4",
+    "url": "/reactnw/static/js/0.b983ff20.chunk.js"
+  },
+  {
+    "revision": "5769966fdf961da0f2f0",
+    "url": "/reactnw/static/js/3.03193204.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactnw/static/js/3.03193204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1817c2041e7b7b25ea75",
+    "url": "/reactnw/static/js/4.80ee130b.chunk.js"
+  },
+  {
+    "revision": "f1baa4946ea71a615beb",
+    "url": "/reactnw/static/js/5.9e2735d6.chunk.js"
+  },
+  {
+    "revision": "b064deb6247167c0ef62",
+    "url": "/reactnw/static/js/6.fba723c3.chunk.js"
+  },
+  {
+    "revision": "8e12c7b803c577e3e659",
+    "url": "/reactnw/static/js/7.e98a7e9d.chunk.js"
+  },
+  {
+    "revision": "a8fac92571d05d63fa80",
+    "url": "/reactnw/static/js/main.fef1deb7.chunk.js"
+  },
+  {
+    "revision": "49650e6c4e7328581ee0",
+    "url": "/reactnw/static/js/runtime-main.8133f053.js"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/reactnw/static/media/preloader.de50e2e9.gif"
+  },
+  {
+    "revision": "a3e369afcda583aa6af1d898d9b14654",
+    "url": "/reactnw/static/media/user.a3e369af.png"
+  }
+]);
